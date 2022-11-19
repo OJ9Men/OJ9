aa015160138614de383c650ca9a80ae0b83a1bd8
Geon Oh <ohhh123@naver.com>
2022-11-19 22:57:19 +0900

    Login씬, Lobby씬 추가
    LoginScene
    - 프로젝트 이름 추가(5G9)
    - 배경 추가
    - Start 버튼 추가 (LobbyScene 로드)
    
    LobbyScene
    - 빈 씬만 추가
    
    + UI::Button을 추가하니 TextMesh도 추가됐음; (잘쓰긴할듯)

---
+++ Assets/Scripts/Login/StartButtonScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButtonScript : MonoBehaviour
+{
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(OnButtonClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnButtonClicked()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
+}
