476135dfd9518d5c958ac1def00d114b5695a52d
Geon Oh <ohhh123@naver.com>
2022-12-03 16:32:03 +0900

    플레이어가 움직일 수 있는 경계 설정

--- Assets/Scripts/Battle/PlayerMovement.cs
+++ Assets/Scripts/Battle/PlayerMovement.cs
@@ -6,11 +6,34 @@ public class PlayerMovement : MonoBehaviour
     private bool canMove;
     private Vector2 playerSize;
     private Rigidbody2D rb;
+
+    public Transform boundaryHolder;
+    Boundary playerBoundary;
+
+    struct Boundary
+    {
+        public float Up, Down, Left, Right;
+
+        public Boundary(float up, float down, float left, float right)
+        {
+            Up = up;
+            Down = down;
+            Left = left;
+            Right = right;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         playerSize = GetComponent<SpriteRenderer>().bounds.extents;
         rb = GetComponent<Rigidbody2D>();
+
+        playerBoundary = new Boundary(
+            boundaryHolder.GetChild(0).position.y,
+            boundaryHolder.GetChild(1).position.y,
+            boundaryHolder.GetChild(2).position.x,
+            boundaryHolder.GetChild(3).position.x
+        );
     }
 
     // Update is called once per frame
@@ -25,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
 
                 // when you capture it
                 if ((mousePos.x >= transform.position.x && mousePos.x < transform.position.x + playerSize.x ||
-                    mousePos.x <= transform.position.x && mousePos.x > transform.position.x -playerSize.x) &&
+                    mousePos.x <= transform.position.x && mousePos.x > transform.position.x - playerSize.x) &&
                     (mousePos.y >= transform.position.y && mousePos.y < transform.position.y + playerSize.y ||
                     mousePos.y <= transform.position.y && mousePos.y > transform.position.y - playerSize.y))
                 {
@@ -39,7 +62,12 @@ public class PlayerMovement : MonoBehaviour
 
             if (canMove)
             {
-                rb.MovePosition(mousePos);
+                Vector2 clampedMousePos = new Vector2(
+                    Mathf.Clamp(mousePos.x, playerBoundary.Left, playerBoundary.Right),
+                    Mathf.Clamp(mousePos.y, playerBoundary.Down, playerBoundary.Up)
+                );
+
+                rb.MovePosition(clampedMousePos);
             }
         }
         else
