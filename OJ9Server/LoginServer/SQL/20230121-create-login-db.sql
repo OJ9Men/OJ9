b28f988222ffe73604eded080317fc5d6a4a9d60
Geon Oh <ohhh123@naver.com>
2023-01-22 00:02:07 +0900

    OJ9-8 login db 생성을 위한 쿼리 작성

---
+++ OJ9Server/LoginServer/SQL/20230121-create-login-db.sql
@@ -0,0 +1,13 @@
+﻿create database login;
+
+create table login.user_temp
+(
+    id       bigint      not null
+        primary key,
+    idString varchar(45) not null,
+    pw       varchar(45) not null,
+    constraint idString_UNIQUE
+        unique (idString)
+)
+    comment 'before using kakao login, temporary applied';
+
